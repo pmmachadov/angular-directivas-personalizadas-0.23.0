37b946ea205490156c745f7efd0527784318ca2e
Pablo Machado <pmmachadov@gmail.com>
2022-12-20 17:53:16 +0100

    .

--- src/app/shared/directives/custom-if.directive.ts
+++ src/app/shared/directives/custom-if.directive.ts
@@ -1,21 +1,21 @@
 import { Directive, Input, TemplateRef, ViewContainerRef  } from '@angular/core';
 
-@Directive({
-  selector: '[customIf]'
+@Directive({  //La directiva customIf se usa para mostrar u ocultar un elemento html
+  selector: '[customIf]'  //El selector de la directiva customIf es customIf
 })
-export class CustomIfDirective {
+export class CustomIfDirective {  //La directiva customIf se usa para mostrar u ocultar un elemento html
 
-  @Input() set customIf( condicion: boolean ) {
-    if ( condicion ) {
-      this.viewContainer.createEmbeddedView( this.templateRef );
-    } else {
-      this.viewContainer.clear();
+  @Input() set customIf( condicion: boolean ) { //El input customIf se usa para mostrar u ocultar un elemento html
+    if ( condicion ) {  //Si el valor del input customIf es true, se muestra el elemento html
+      this.viewContainer.createEmbeddedView( this.templateRef );  //createEmbeddedView es para crear una vista embebida en el elemento html que tenga la directiva customIf
+    } else {  //Si el valor del input customIf es false, se oculta el elemento html
+      this.viewContainer.clear(); //clear es para limpiar el elemento html que tenga la directiva customIf
     }
   }
 
-  constructor(
-    private templateRef: TemplateRef<HTMLElement>,
-    private viewContainer: ViewContainerRef
+  constructor(  //Inyectamos el TemplateRef y el ViewContainerRef para poder manipular el elemento html que tenga la directiva customIf
+    private templateRef: TemplateRef<HTMLElement>,  //TemplateRef es para obtener la referencia al template del elemento html que tenga la directiva customIf
+    private viewContainer: ViewContainerRef //ViewContainerRef es para obtener la referencia al elemento html que tenga la directiva customIf
   ) {}
 
 
--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,18 +1,18 @@
 import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
-@Directive({
-  selector: '[error-msg]'
+@Directive({  //La directiva error-msg se usa para mostrar un mensaje de error en un elemento html
+  selector: '[error-msg]' //El selector de la directiva error-msg es error-msg
 })
 export class ErrorMsgDirective implements OnInit, OnChanges { //OnChanges es para detectar cambios en los inputs de la directiva y OnInit es para detectar cuando se crea la directiva en el template del componente agregar.component.html y poder manipular el elemento html que tenga la directiva error-msg
 
-  private _color: string = 'red';
-  private _mensaje: string = 'Este campo es requerido';
+  private _color: string = 'red'; //Este input se usa para cambiar el color del mensaje de la directiva
+  private _mensaje: string = 'Este campo es requerido'; //Este input se usa para cambiar el valor del mensaje de la directiva
 
-  htmlElement: ElementRef<HTMLElement>;
+  htmlElement: ElementRef<HTMLElement>; //Inyectamos el ElementRef para poder manipular el elemento html que tenga la directiva error-msg en el template del componente agregar.component.html
 
-  @Input() set color( valor: string) {
-    this._color = valor;
-    this.setColor();
+  @Input() set color( valor: string) {  //Este input se usa para cambiar el color del mensaje de la directiva
+    this._color = valor;  //El valor del input color se asigna a la variable _color
+    this.setColor();  //Este metodo se ejecuta cuando se crea la directiva y cuando se cambia el valor del input color
   }
 
   @Input() set mensaje( valor: string ) { //Este input se usa para cambiar el valor del mensaje de la directiva
@@ -20,11 +20,11 @@ export class ErrorMsgDirective implements OnInit, OnChanges { //OnChanges es par
     this.setMensaje();  //Este metodo se ejecuta cuando se crea la directiva y cuando se cambia el valor del input mensaje
   }
 
-  @Input() set valido( valor: boolean ) {
-    if( valor ) {
-      this.htmlElement.nativeElement.classList.add('hidden');
-    } else {
-      this.htmlElement.nativeElement.classList.remove('hidden');
+  @Input() set valido( valor: boolean ) { //Este input se usa para mostrar u ocultar el mensaje de la directiva
+    if( valor ) { //Si el valor del input valido es true, se oculta el mensaje de la directiva
+      this.htmlElement.nativeElement.classList.add('hidden'); //classList es para obtener la lista de clases del elemento html. add es para agregar una clase al elemento html
+    } else {  //Si el valor del input valido es false, se muestra el mensaje de la directiva
+      this.htmlElement.nativeElement.classList.remove('hidden');  //classList es para obtener la lista de clases del elemento html. remove es para remover una clase del elemento html
     }
   }
 
@@ -48,7 +48,7 @@ export class ErrorMsgDirective implements OnInit, OnChanges { //OnChanges es par
 
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  //Este metodo se ejecuta cuando se crea la directiva en el template del componente agregar.component.html y poder manipular el elemento html que tenga la directiva error-msg
 
     // console.log(this.color); // undefined
     // console.log(this.mensaje); // undefined
@@ -57,15 +57,15 @@ export class ErrorMsgDirective implements OnInit, OnChanges { //OnChanges es par
     this.setMensaje();
   }
 
-  setEstilo(): void {
-    this.htmlElement.nativeElement.classList.add('form-text');
+  setEstilo(): void { // Establece el estilo del elemento html que tenga la directiva error-msg
+    this.htmlElement.nativeElement.classList.add('form-text'); //classList es para obtener la lista de clases del elemento html. add es para agregar una clase al elemento html.
   }
 
-  setColor(): void {
-    this.htmlElement.nativeElement.style.color = this._color;
+  setColor(): void { //Establece el color del elemento html que tenga la directiva error-msg
+    this.htmlElement.nativeElement.style.color = this._color; //style es para obtener el estilo del elemento html. color es para obtener el color del elemento html
   }
 
-  setMensaje(): void {  //Este metodo se ejecuta cuando se crea la directiva y cuando se cambia el valor del input mensaje
+  setMensaje(): void { //Este metodo se ejecuta cuando se crea la directiva y cuando se cambia el valor del input mensaje
     this.htmlElement.nativeElement.innerText = this._mensaje; //innerText es para obtener el texto del elemento html
   }
 
